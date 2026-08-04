d6a2e0bdc1384d9cc3606d99259d617ab15c73b6
Navya Prasanna <navyadogganal@gmail.com>
2026-08-04 20:11:36 +0530

    Day 7 completed

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("changeBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function() {
+    message.innerHTML = "Thank you for visiting my portfolio!";
+});
